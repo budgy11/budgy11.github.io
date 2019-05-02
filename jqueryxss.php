a88776410bab15cc999493c795a23826ad1cc2fa
Semenok,Victor <victor.semenok@egov.com>
2019-05-02 13:18:24 -0500

    Init commit

--- jqueryxss.php
+++ jqueryxss.php
@@ -1,1 +1,4 @@
-alert(document.domain);
+<html><head><link rel="alternate stylesheet" type="text/css" 
+href="resource://content-accessible/plaintext.css" title="Wrap Long 
+Lines"></head><body><pre>alert(document.domain);
+</pre></body></html>
